bd34727ce1d27fa610dc8318680db9486a7637cf
Rosen Ivanov <rosen.p.ivanov@gmail.com>
2018-04-29 10:54:18 +0300

    initial

--- crawl.js
+++ crawl.js
@@ -1,35 +1,36 @@
 const axios = require('axios');
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-const api_key='f9ba0ef67b79d86f09e47b9454c3a8f2';
-var sleep = require('sleep');
+const api_key = require('./local.config.js');
+//const sleep = require('sleep');
 
 const movieList = (maxMovies=10000) => {
-    const movieIds = [];
-    const numPages = maxMovies / 20
-    for( var page = 1 ; page < numPages; page++ ) {
-      console.log(page);
-       axios.get('/movie/top_rated', {params:{page, api_key}})
-        .then(response => {console.log('movies page ' + page, response)
-         if (response.headers['x-ratelimit-remaining'] < 10) {
-           console.log('do sleep')
-                        sleep.sleep(3)
-                      }
+  const movieIds = [];
+  const numPages = maxMovies / 20;
+  for (let page = 1; page < numPages; page++) {
+    console.log(page);
+    axios.get('/movie/top_rated', { params: { page, api_key } })
+      .then(response => {
+        console.log(`movies page ${page}`, response);
+        if (response.headers['x-ratelimit-remaining'] < 10) {
+          console.log('do sleep');
+          //sleep.sleep(3);
+        }
       })
-        .catch(error => console.log(error))
-    }
+      .catch(error => console.log(error));
+  }
 
- //    for page in range(1, numPages + 1):
- //        httpResp = tmdb_api.get(url, params={'page': page})
- //        try:
- // if int(httpResp.headers['x-ratelimit-remaining']) < 10:
- //                time.sleep(3)
- //        except Exception as e:
- //            print e
- //        jsonResponse = json.loads(httpResp.text)
- //        movies = jsonResponse['results']
- //        for movie in movies:
- //            movieIds.append(movie['id'])
- //    return movieIds
+  //    for page in range(1, numPages + 1):
+  //        httpResp = tmdb_api.get(url, params={'page': page})
+  //        try:
+  // if int(httpResp.headers['x-ratelimit-remaining']) < 10:
+  //                time.sleep(3)
+  //        except Exception as e:
+  //            print e
+  //        jsonResponse = json.loads(httpResp.text)
+  //        movies = jsonResponse['results']
+  //        for movie in movies:
+  //            movieIds.append(movie['id'])
+  //    return movieIds
 };
 
 movieList();
--- initES.js
+++ initES.js
@@ -4,49 +4,42 @@ axios.defaults.baseURL = 'http://localhost:9200';
 const reindex = (analysisSettings={}, mappingSettings={}, movieDict={}) => {
 
   const settings = {
-    "settings": {
-      "number_of_shards": 1,
-      "index": {"analysis" : analysisSettings}
+    settings: {
+      number_of_shards: 1,
+      index: { analysis: analysisSettings }
     }
-}
+  };
 
-if (mappingSettings)
-  settings['mappings'] = mappingSettings
-axios.delete('/tmdb', {})
-  .then(response => console.log('index deleted'))
-  .catch(error => console.log(error))
-  .then(() => {
-    console.log('lets create new index')
-    axios.put('/tmdb', settings)
-     .then(response => console.log('index created', response))
-     .catch(error => console.log(error))
-     .then(()=> {
-
-       let bulkMovies = ""
-       for id, movie in movieDict.iteritems():
-           addCmd = {"index": {"_index": "tmdb",
-                               "_type": "movie",
-                               "_id": movie["id"]}
-                             }
-           bulkMovies += json.dumps(addCmd) + "\n" + json.dumps(movie) + "\n"
-
-
-       axios.post('/_bulk"', settings)
+  if (mappingSettings)
+    settings.mappings = mappingSettings;
+  axios.delete('/tmdb', {})
+    .then(response => console.log(`index deleted${response}`))
+    .catch(error => console.log(error))
+    .then(() => {
+      console.log('lets create new index');
+      axios.put('/tmdb', settings)
         .then(response => console.log('index created', response))
         .catch(error => console.log(error))
+        .then(() => {
 
+          // let bulkMovies = ""
+          // for id, movie in movieDict.iteritems():
+          //     addCmd = {"index": {"_index": "tmdb",
+          //                         "_type": "movie",
+          //                         "_id": movie["id"]}
+          //                       }
+          //     bulkMovies += json.dumps(addCmd) + "\n" + json.dumps(movie) + "\n"
 
-     });
-  })
-}
+          axios.post('/_bulk"', settings)
+            .then(response => console.log('index created', response))
+            .catch(error => console.log(error));
 
-
-
-
+        });
+    });
+};
 
 reindex();
 //resp = http.put("/tmdb",
-
 
 // var data=json.dumps(settings))
 // var bulkMovies = ""
