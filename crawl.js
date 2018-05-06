30fa55e0ddbffd721a546ca28b44a12483054c6e
Rosen Ivanov <rosen.p.ivanov@gmail.com>
2018-05-06 10:54:30 +0300

    extract

--- crawl.js
+++ crawl.js
@@ -5,20 +5,27 @@ const api_key = require('./local.config.js').api_key;
 console.log(api_key);
 
 const movieList = (maxMovies=10000) => {
-  const movieIds = [];
+  
   const numPages = maxMovies / 20;
 
   const nextRequest = page => {
+    
     if (page>numPages) {
       console.log('finished');
     } else {
       axios.get('/movie/top_rated', { params: { page, api_key } })
         .then(response => {
-          console.log(`movies page ${page}`, response);
-          console.log(`movies page ${page}`, response.results);
+          const movieIds = [];
+          console.log(`movies page ${page}`);
+          //console.log(JSON.stringify(response.data.results));
+          response.data.results.forEach(element => {
+            movieIds.push(element.id);
+          });
+          
+          console.log('movieIds', movieIds);
+          //TODO extract(movieIds) -> index
 
           if (response.headers['x-ratelimit-remaining'] < 5) {
-            console.log('do sleep');
             setTimeout(nextRequest(page+1), 3000);
           } else {
             nextRequest(page+1);
@@ -29,34 +36,29 @@ const movieList = (maxMovies=10000) => {
   };
 
   nextRequest(1);
+  console.log('movieIds', movieIds);
 
-//  for (let page = 1; page < numPages; page++) {
-  //  console.log(page);
-//    listRequests.push(axios.get('/movie/top_rated', { params: { page, api_key } }));
-  // .then(response => {
-  //   console.log(`movies page ${page}`, response);
-  //   console.log(`movies page ${page}`, response.results);
-  //   throw "gg";
-  //   if (response.headers['x-ratelimit-remaining'] < 10) {
-  //     console.log('do sleep');
-  //     //sleep.sleep(3);
-  //   }
-  // })
-  // .catch(error => {console.log(error); throw "gg";});
+ cosnt extract =  (movieIds=[], numMovies=10000) => {
+   movieIds.map(movieId => {
+     axios.get('/movie/' + movieId  + '/' , { params: { api_key } })
+     .then(response => {      
+      //console.log(JSON.stringify(response.data.results));
+       getCastAndCrew(movieId, movie)           
+
+      if (response.headers['x-ratelimit-remaining'] < 5) {
+        setTimeout(nextRequest(page+1), 3000);
+      } else {
+        nextRequest(page+1);
+      }
+    })
+    .catch(error => console.log(error));
+    
+    //if int(httpResp.headers['x-ratelimit-remaining']) < 10:
+    
+    
+    
+  });
 };
-
-  //    for page in range(1, numPages + 1):
-  //        httpResp = tmdb_api.get(url, params={'page': page})
-  //        try:
-  // if int(httpResp.headers['x-ratelimit-remaining']) < 10:
-  //                time.sleep(3)
-  //        except Exception as e:
-  //            print e
-  //        jsonResponse = json.loads(httpResp.text)
-  //        movies = jsonResponse['results']
-  //        for movie in movies:
-  //            movieIds.append(movie['id'])
-  //    return movieIds
-//};
+  
 
 movieList();
