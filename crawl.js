6b0b58fe2db60b1125e84ffd96ad7251286e1bd2
Rosen Ivanov <rosen.p.ivanov@gmail.com>
2018-05-20 00:47:09 +0300

    working

--- crawl.js
+++ crawl.js
@@ -10,12 +10,12 @@ const sleep = async fn => {
   await timeout(10000);
   console.log('after timeout');
 
-  return fn();
+  return await fn();
 };
 
-const getCastAndCrew = (movieId, movie) => {
+const getCastAndCrew = async (movieId, movie) => {
   console.log(`getCastAndCrew for ${movieId}`);
-  axios.get(`/movie/${movieId}/credits` , { params: { api_key } })
+  await axios.get(`/movie/${movieId}/credits` , { params: { api_key } })
     .then(response => {
       //console.log(JSON.stringify(response.data));
       const credits = response.data;
@@ -32,19 +32,19 @@ const getCastAndCrew = (movieId, movie) => {
     })
     .catch(error => console.log(`getCastAndCrew: ${error}`));
 };
-const extract = (movieIds=[]) => {
+const extract = async (movieIds=[]) => {
   console.log(`extract for ${movieIds}`);
-  movieIds.map(async movieId => {
+  for (const movieId of movieIds) {
     await axios.get(`/movie/${movieId}` , { params: { api_key } })
-      .then(response => {
+      .then(async response => {
         //console.log('movie:', JSON.stringify(response.data));
         const movie = response.data;
         console.log(`movieId: ${movieId}-${response.headers['x-ratelimit-remaining']}`);
-        if (response.headers['x-ratelimit-remaining'] < 20) {
+        if (response.headers['x-ratelimit-remaining'] < 5) {
           console.log(`will throttle 2 ${response.headers['x-ratelimit-remaining']}`);
-          sleep(() => getCastAndCrew(movieId, movie));
+          await sleep(async () => await getCastAndCrew(movieId, movie));
         } else
-          getCastAndCrew(movieId, movie);
+          await getCastAndCrew(movieId, movie);
       //  return movie;
       })
     //  .then(() => {
@@ -53,57 +53,52 @@ const extract = (movieIds=[]) => {
     //  })
       .catch(error => console.log(error));
 
-  //if int(httpResp.headers['x-ratelimit-remaining']) < 20:
+  //if int(httpResp.headers['x-ratelimit-remaining']) < 5:
 
-  });
-};
-
-const movieList = (maxMovies=10000) => {
-
-  const numPages = maxMovies / 20;
-
-  const nextRequest = page => {
-
-    if (page>numPages) {
-      console.log('finished');
-    } else {
-      axios.get('/movie/top_rated', { params: { page, api_key } })
-        .then(response => {
-          const movieIds = [];
-          console.log(`movies page ${page}`);
-          //console.log(JSON.stringify(response.data.results));
-          response.data.results.forEach(element => {
-            movieIds.push(element.id);
-          });
-
-          console.log('movieIds', movieIds);
-          console.log('movieIds.length', movieIds.length);
-          if (response.headers['x-ratelimit-remaining'] < 20) {
-            sleep(() => extract(movieIds));
-          } else {
-            extract(movieIds);
-          }
-
-          // if (response.headers['x-ratelimit-remaining'] < 20) {
-          //   setTimeout(nextRequest(page+1), 3000);
-          // } else {
-          //   nextRequest(page+1);
-          // }
-          return response;
-        })
-        .then(response => {
-          if (response.headers['x-ratelimit-remaining'] < 20) {
-            console.log(`will throttle 1 ${response.headers['x-ratelimit-remaining']}`);
-            sleep(() => nextRequest(page+1));
-          } else {
-            nextRequest(page+1);
-          }
-        })
-        .catch(error => console.log(error));
-    }
   };
 
-  nextRequest(1);
+  return 1;
 };
 
-movieList(20);
+const movieList = async (maxMovies=10000) => {
+
+  const numPages = maxMovies / 20;
+  for (let page=1; page <= numPages; page++) {
+    await axios.get('/movie/top_rated', { params: { page, api_key } })
+      .then(async response => {
+        const movieIds = [];
+        console.log(`movies page ${page}`);
+        //console.log(JSON.stringify(response.data.results));
+        response.data.results.forEach(element => {
+          movieIds.push(element.id);
+        });
+
+        console.log('movieIds', movieIds);
+        console.log('movieIds.length', movieIds.length);
+        if (response.headers['x-ratelimit-remaining'] < 5) {
+          console.log('will throttle extract');
+          await sleep(async () => await extract(movieIds));
+        } else {
+          await extract(movieIds);
+        }
+
+        // if (response.headers['x-ratelimit-remaining'] < 5) {
+        //   setTimeout(nextRequest(page+1), 3000);
+        // } else {
+        //   nextRequest(page+1);
+        // }
+        return response;
+      })
+      .then(async response => {
+        if (response.headers['x-ratelimit-remaining'] < 5) {
+          console.log(`will throttle 1 ${response.headers['x-ratelimit-remaining']}`);
+
+          return await sleep(() => {});
+        }
+      })
+      .catch(error => console.log(error));
+
+  }
+};
+
+movieList(1000);
